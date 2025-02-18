e8f6cf43eb286601897df5771c36f97303dccf50
pgm-serdkara <serdar.karaman@student.arteveldehs.be>
2025-02-18 09:48:14 +0100

    Cloned repo earlier

--- src/app.js
+++ src/app.js
@@ -5,13 +5,21 @@ import path from "path";
 // create an instance of express
 const app = express();
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dinosaurs are awesome!",
+    content:
+      "Dinosaurs are a diverse group of reptiles of the clade Dinosauria. They first appeared during the Triassic period, between 243 and 233.23 million years ago, although the exact origin and timing of the evolution of dinosaurs is the subject of active research.",
+  });
 });
 
 // start the server, listen on port defined in .env file
